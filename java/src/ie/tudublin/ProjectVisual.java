c16998b9c000d72f79737f780d17706410cea8fd
Cathal Small <cathal.small1@gmail.com>
2023-04-05 19:43:54 +0100

    spiral semi-finished

--- java/src/ie/tudublin/ProjectVisual.java
+++ java/src/ie/tudublin/ProjectVisual.java
@@ -2,22 +2,31 @@ package ie.tudublin;
 
 public class ProjectVisual extends Visual {
 
-    private int mode = 0;
+    //private int mode = 0;
+    
     
 
     public void settings(){
-		size(1024, 1000, P3D);
+		//size(1024, 1000, P3D);
+        fullScreen(P3D);
 	}
 
 	public void setup(){
+        colorMode(HSB,360,100,100);
 		startMinim();
 		loadAudio("Bee Gees - Stayin' Alive (Official Music Video).wav");
 		startListening();
 	}
+    
+    float distance = 0;
+    float distanceChange = 0.001f;
 
     Poly spiral = new Spiral(this);
 
+
 	public void draw(){
+
+        stroke(255,255,255);
     
         background(0);
         try
@@ -34,7 +43,9 @@ public class ProjectVisual extends Visual {
 
         // Call this is you want to get the average amplitude
         calculateAverageAmplitude();        
-        spiral.render(); 
+       
+       spiral.render();
     }   
     
 }
+    
--- java/src/ie/tudublin/Spiral.java
+++ java/src/ie/tudublin/Spiral.java
@@ -4,18 +4,31 @@ import processing.core.PApplet;
 
 public class Spiral extends Poly{
 
+   float theta;
+   float radius;
+
     public Spiral(ProjectVisual v){
         super(v);
     }
 
+   
 
     @Override
     public void render(){
-        v.calculateAverageAmplitude();
-       v.background(0);
-        v.noFill();
-        v.stroke(PApplet.map(v.getSmoothedAmplitude(), 0, 1, 0, 255), 255, 255);
-        v.circle(v.width /2,v.height /2, 500);
+        //NOTE: ON BEAT SWITCH BETWEEN FILL AND NOFILL
+        for(int i = 0; i< v.getAudioBuffer().size(); i++){
+            v.pushMatrix();
+            //v.noFill();
+            float c = PApplet.map(i, 0, v.getAudioBuffer().size() , 0, 255);
+            v.fill(c,255,255);
+            v.stroke(c,255,255);
+            theta = PApplet.map(i, 0, v.getAudioBuffer().size(),0 , PApplet.TWO_PI*15);
+            radius = PApplet.map(i, 0, v.getAudioBuffer().size(), 0, 800);
+            float x = PApplet.sin(theta) * radius; // calculates the x co-ordinate of the next circle within the spiral
+            float y = PApplet.cos(theta) * radius; // calculates the y co-ordinate of the next circle within the spiral
+            v.circle(x+v.width/2, y+v.height/2, v.getAudioBuffer().get(i) * 150);
+            v.popMatrix();
+        }
 
     }
 
