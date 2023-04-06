a9e47ab0f7b632b22b2050b24ac601907eed5e85
Cathal Small <cathal.small1@gmail.com>
2023-04-06 16:43:48 +0100

    Sampel transitions created

--- java/src/ie/tudublin/Test.java
+++ java/src/ie/tudublin/Test.java
@@ -12,12 +12,14 @@ public class Test extends Poly{
     public void render(){
         for (int i =0; i< v.getAudioBuffer().size(); i++){
             v.pushMatrix();
-            float c = PApplet.map(i, 0, v.getAudioBuffer().size() , 0, 255);
-            //float c = PApplet.map(v.getAudioBuffer().get(i), -1, 0 , 0, 255);
+            //float c = PApplet.map(i, 0, v.getAudioBuffer().size() , 0, 255);
+            float c = PApplet.map(v.getAudioBuffer().get(i), -1, 0 , 0, 255);
             v.noFill();
             v.stroke(c,255,255);
-            v.circle((v.getAudioBuffer().get(i) * 2) + v.width/2, (v.getAudioBuffer().get(i) * 2) + v.height/2, v.getAudioBuffer().get(i) * 1000);
-            //v.circle(v.width/2,v.height/2, v.getSmoothedAmplitude() * 1000);
+            //v.circle((v.getAudioBuffer().get(i) * 2) + v.width/2, (v.getAudioBuffer().get(i) * 2) + v.height/2, v.getAudioBuffer().get(i) * 1000);
+            v.circle(v.width/2,v.height/2, v.getSmoothedAmplitude() * i*i); // Idea for potential transition between viuals
+            //v.translate(v.width/2, v.height/2);
+            //v.box(v.getSmoothedAmplitude() * i*i); // Idea for potential transition between viuals
             v.popMatrix();
 
         }
