3db2f519b1e35c74736a5bf7da82a42e411777b3
lilja <liljaobyrne@gmail.com>
2023-05-02 20:26:34 +0100

    finishing touches

--- java/src/ie/tudublin/kalidascope.java
+++ java/src/ie/tudublin/kalidascope.java
@@ -1,16 +1,8 @@
 package ie.tudublin;
-import java.util.ArrayList;
-
-import com.jogamp.opengl.util.packrect.Rect;
-
-import ddf.minim.analysis.BeatDetect;
 import processing.core.PApplet;
-import processing.core.PVector;
 
 public class kalidascope extends Poly
 {
-    float start = v.millis();
-
     public kalidascope(ProjectVisual v) {
         super(v);
     }
@@ -21,32 +13,28 @@ public class kalidascope extends Poly
         v.background(0);
         v.stroke(255);
 
-        middle();
+        kal();
     }
 
-   
-
-    void middle()
+    void kal()
     {
+        //strokeweight starting point
         float sw = 0.7f;
         v.translate(v.width/2, v.height/2);
         int count = 0;
 
-        
-
         for(int i = 0; i < 2000;i+=60)
         {
-            //rotate entire shape
-            
             float radius = 100+i;
-            //map of bluish pinkish colours
+            //rotate entire shape
             v.rotate(PApplet.radians(v.frameCount));
+            //map of bluish pinkish colours
             float c = PApplet.map(count, 0, v.getAudioBuffer().size() , 200, 400);
             
             v.beginShape();
-            
-            
-            //create pentagons
+            //create multiple octogons as one shape
+            //until they are in a circular pattern.
+            // div twopi for how many sides in the shape, 8 = octogon
             for(float a = 0; a < v.TWO_PI; a+=v.TWO_PI/8)
             {
                 float x = radius * v.cos(a);
@@ -57,21 +45,13 @@ public class kalidascope extends Poly
                     v.strokeWeight(sw);
                     sw+=0.02;
                 }
-               
                 v.noFill();
                 v.stroke(c, 150, 200);
                 v.vertex(x*v.getSmoothedAmplitude(), y*v.getSmoothedAmplitude());
-                //v.circle(x*v.getSmoothedAmplitude()+100, y*v.getSmoothedAmplitude()+100, 20);
-                //v.rect(x*v.getSmoothedAmplitude()+100, y*v.getSmoothedAmplitude()+100, 20,20);
                 count++;
             }
-            
             v.endShape();
-        }
-
-        
+        }        
     }
-
-   
 }
 
